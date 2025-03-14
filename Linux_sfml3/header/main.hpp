a42e069bff8ea5c26bfef1abbacbe4466121ce96
Sacha Trouve <sacha.trouve@etu.unilim.fr>
2025-03-14 22:30:55 +0100

    load project

---
+++ Linux_sfml3/header/main.hpp
@@ -0,0 +1,10 @@
+#ifndef MAIN
+#define MAIN
+
+#include <SFML/Graphics.hpp>
+
+#define SCREEN sf::Vector2i(sf::VideoMode::getFullscreenModes().at(0).size)
+
+sf::Color color();
+
+#endif
---
+++ Windows_sfml2/header/main.hpp
@@ -0,0 +1,9 @@
+#ifndef MAIN
+#define MAIN
+
+#define SCREEN_X (int)sf::VideoMode::getDesktopMode().width
+#define SCREEN_Y (int)sf::VideoMode::getDesktopMode().height
+
+sf::Color color();
+
+#endif
